e664e004d624df00ed34c45b104e47e6673705f4
Peter Gribanov <info@peter-gribanov.ru>
2014-04-24 14:34:48 +0400

    create bundle src

---
+++ src/AnimeDb/Bundle/AniDbFillerBundle/AnimeDbAniDbFillerBundle.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * AnimeDb package
+ *
+ * @package   AnimeDb
+ * @author    Peter Gribanov <info@peter-gribanov.ru>
+ * @copyright Copyright (c) 2011, Peter Gribanov
+ * @license   http://opensource.org/licenses/GPL-3.0 GPL v3
+ */
+
+namespace AnimeDb\Bundle\AniDbFillerBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Bundle
+ *
+ * @package AnimeDb\Bundle\AniDbFillerBundle
+ * @author  Peter Gribanov <info@peter-gribanov.ru>
+ */
+class AnimeDbAniDbFillerBundle extends Bundle
+{
+}
