1318de7bee7dfcf3c2c13e32ad8bc1bf87d9fe28
Peter Gribanov <info@peter-gribanov.ru>
2014-04-24 17:03:15 +0400

    create empty command for sync titles

---
+++ src/AnimeDb/Bundle/AniDbFillerBundle/Command/SyncTitlesCommand.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * AnimeDb package
+ *
+ * @package   AnimeDb
+ * @author    Peter Gribanov <info@peter-gribanov.ru>
+ * @copyright Copyright (c) 2011, Peter Gribanov
+ * @license   http://opensource.org/licenses/GPL-3.0 GPL v3
+ */
+
+namespace AnimeDb\Bundle\AniDbFillerBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use AnimeDb\Bundle\AniDbFillerBundle\Entity\Title;
+
+/**
+ * Sync list of titles from AniDB.net
+ *
+ * @package AnimeDb\Bundle\AniDbFillerBundle\Command
+ * @author  Peter Gribanov <info@peter-gribanov.ru>
+ */
+class SyncTitlesCommand extends ContainerAwareCommand
+{
+    /**
+     * (non-PHPdoc)
+     * @see Symfony\Component\Console\Command.Command::configure()
+     */
+    protected function configure()
+    {
+        $this->setName('animedb:sync-titles')
+            ->setDescription('Sync list of titles from AniDB.net');
+    }
+
+    /**
+     * (non-PHPdoc)
+     * @see Symfony\Component\Console\Command.Command::execute()
+     */
+    protected function execute(InputInterface $input, OutputInterface $output) {
+    }
+}
