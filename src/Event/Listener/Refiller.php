6f59f7c892d6b40c8a6189f6351aa6c6ef626ecc
Peter Gribanov <info@peter-gribanov.ru>
2014-11-14 15:35:27 +0300

    add event listere for refill item after add

---
+++ src/Event/Listener/Refiller.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * AnimeDb package
+ *
+ * @package   AnimeDb
+ * @author    Peter Gribanov <info@peter-gribanov.ru>
+ * @copyright Copyright (c) 2011, Peter Gribanov
+ * @license   http://opensource.org/licenses/GPL-3.0 GPL v3
+ */
+
+namespace AnimeDb\Bundle\AniDbFillerBundle\Event\Listener;
+
+use AnimeDb\Bundle\AniDbFillerBundle\Service\Refiller as RefillerService;
+use AnimeDb\Bundle\AniDbFillerBundle\Service\Filler;
+use AnimeDb\Bundle\CatalogBundle\Event\Storage\AddNewItem;
+use AnimeDb\Bundle\CatalogBundle\Event\Storage\StoreEvents;
+
+/**
+ * Refiller for new item
+ *
+ * @package AnimeDb\Bundle\AniDbFillerBundle\Event\Listener
+ * @author  Peter Gribanov <info@peter-gribanov.ru>
+ */
+class Refiller
+{
+    /**
+     * Refiller
+     *
+     * @var \AnimeDb\Bundle\AniDbFillerBundle\Service\Refiller
+     */
+    protected $refiller;
+
+    /**
+     * Filler
+     *
+     * @var \AnimeDb\Bundle\AniDbFillerBundle\Service\Filler
+     */
+    protected $filler;
+
+    /**
+     * Construct
+     *
+     * @param \AnimeDb\Bundle\AniDbFillerBundle\Service\Refiller $refiller
+     * @param \AnimeDb\Bundle\AniDbFillerBundle\Service\Filler $filler
+     */
+    public function __construct(RefillerService $refiller, Filler $filler)
+    {
+        $this->refiller = $refiller;
+        $this->filler = $filler;
+    }
+
+    /**
+     * On add new item
+     *
+     * @param \AnimeDb\Bundle\CatalogBundle\Event\Storage\AddNewItem $event
+     */
+    public function onAddNewItem(AddNewItem $event)
+    {
+        $item = $event->getItem();
+        if (!$event->getFillers()->contains($this->filler) && ($url = $this->refiller->getSourceForFill($item))) {
+            $new_item = $this->filler->fill(['url' => $url]);
+
+            // fill item
+            if (!$item->getDateEnd()) {
+                $item->setDateEnd($new_item->getDateEnd());
+            }
+            if (!$item->getDatePremiere()) {
+                $item->setDatePremiere($new_item->getDatePremiere());
+            }
+            if (!$item->getEpisodes()) {
+                $item->setEpisodes($new_item->getEpisodes());
+            }
+            if (!$item->getEpisodesNumber()) {
+                $item->setEpisodesNumber($new_item->getEpisodesNumber());
+            }
+            if (!$item->getSummary()) {
+                $item->setSummary($new_item->getSummary());
+            }
+            foreach ($new_item->getGenres() as $genre) {
+                $item->addGenre($genre);
+            }
+            foreach ($new_item->getNames() as $name) {
+                $item->addName($name);
+            }
+            foreach ($new_item->getSources() as $source) {
+                $item->addSource($source);
+            }
+
+            $event->addFiller($this->filler);
+            // resend event
+            $event->getDispatcher()->dispatch(StoreEvents::ADD_NEW_ITEM, clone $event);
+            $event->stopPropagation();
+        }
+    }
+}
