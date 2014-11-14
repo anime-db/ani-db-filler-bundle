034452ece57f1863f18247713cce689b9a3cbab7
Peter Gribanov <info@peter-gribanov.ru>
2014-11-14 16:52:41 +0300

    remove not used class

--- src/Controller/MediaController.php
+++ src/Controller/MediaController.php
@@ -12,7 +12,6 @@ namespace AnimeDb\Bundle\AniDbFillerBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 
 /**
  * Media
